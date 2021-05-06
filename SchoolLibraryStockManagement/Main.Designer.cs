df57ccfe35d6880da99407b89f77f9878eb3e41b
Jannat Almgawshi <j.m.almgawshi@gmail.com>
2021-05-06 13:06:21 +0200

    Main UI update

--- SchoolLibraryStockManagement/Main.Designer.cs
+++ SchoolLibraryStockManagement/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -52,137 +53,99 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 39);
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(249, 39);
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(484, 39);
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(22, 196);
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             // 
             // lbl_products
             // 
-            this.lbl_products.AutoSize = true;
-            this.lbl_products.Location = new System.Drawing.Point(54, 92);
+            resources.ApplyResources(this.lbl_products, "lbl_products");
             this.lbl_products.Name = "lbl_products";
-            this.lbl_products.Size = new System.Drawing.Size(49, 13);
-            this.lbl_products.TabIndex = 4;
-            this.lbl_products.Text = "Products";
             this.lbl_products.Click += new System.EventHandler(this.lbl_products_Click);
             // 
             // lbl_categories
             // 
-            this.lbl_categories.AutoSize = true;
-            this.lbl_categories.Location = new System.Drawing.Point(275, 92);
+            resources.ApplyResources(this.lbl_categories, "lbl_categories");
             this.lbl_categories.Name = "lbl_categories";
-            this.lbl_categories.Size = new System.Drawing.Size(57, 13);
-            this.lbl_categories.TabIndex = 5;
-            this.lbl_categories.Text = "Categories";
             this.lbl_categories.Click += new System.EventHandler(this.lbl_categories_Click);
             // 
             // lbl_users
             // 
-            this.lbl_users.AutoSize = true;
-            this.lbl_users.Location = new System.Drawing.Point(520, 92);
+            resources.ApplyResources(this.lbl_users, "lbl_users");
             this.lbl_users.Name = "lbl_users";
-            this.lbl_users.Size = new System.Drawing.Size(34, 13);
-            this.lbl_users.TabIndex = 6;
-            this.lbl_users.Text = "Users";
             this.lbl_users.Click += new System.EventHandler(this.lbl_users_Click);
             // 
             // lbl_orders
             // 
-            this.lbl_orders.AutoSize = true;
-            this.lbl_orders.Location = new System.Drawing.Point(54, 249);
+            resources.ApplyResources(this.lbl_orders, "lbl_orders");
             this.lbl_orders.Name = "lbl_orders";
-            this.lbl_orders.Size = new System.Drawing.Size(38, 13);
-            this.lbl_orders.TabIndex = 7;
-            this.lbl_orders.Text = "Orders";
             this.lbl_orders.Click += new System.EventHandler(this.lbl_orders_Click);
             // 
             // lbl_reports
             // 
-            this.lbl_reports.AutoSize = true;
-            this.lbl_reports.Location = new System.Drawing.Point(281, 249);
+            resources.ApplyResources(this.lbl_reports, "lbl_reports");
             this.lbl_reports.Name = "lbl_reports";
-            this.lbl_reports.Size = new System.Drawing.Size(44, 13);
-            this.lbl_reports.TabIndex = 9;
-            this.lbl_reports.Text = "Reports";
             this.lbl_reports.Click += new System.EventHandler(this.lbl_reports_Click);
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(249, 196);
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox5.TabIndex = 8;
             this.pictureBox5.TabStop = false;
             // 
             // lbl_stock_management
             // 
-            this.lbl_stock_management.AutoSize = true;
-            this.lbl_stock_management.Location = new System.Drawing.Point(484, 249);
+            resources.ApplyResources(this.lbl_stock_management, "lbl_stock_management");
             this.lbl_stock_management.Name = "lbl_stock_management";
-            this.lbl_stock_management.Size = new System.Drawing.Size(100, 13);
-            this.lbl_stock_management.TabIndex = 11;
-            this.lbl_stock_management.Text = "Stock Management";
             this.lbl_stock_management.Click += new System.EventHandler(this.lbl_settings_Click);
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(484, 196);
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox6.TabIndex = 10;
             this.pictureBox6.TabStop = false;
             // 
             // btn_logout
             // 
-            this.btn_logout.Location = new System.Drawing.Point(523, 309);
+            resources.ApplyResources(this.btn_logout, "btn_logout");
+            this.btn_logout.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_logout.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_logout.Name = "btn_logout";
-            this.btn_logout.Size = new System.Drawing.Size(75, 23);
-            this.btn_logout.TabIndex = 12;
-            this.btn_logout.Text = "Logout";
-            this.btn_logout.UseVisualStyleBackColor = true;
+            this.btn_logout.UseVisualStyleBackColor = false;
             this.btn_logout.Click += new System.EventHandler(this.btn_logout_Click);
             // 
             // btn_settings
             // 
-            this.btn_settings.Location = new System.Drawing.Point(442, 309);
+            resources.ApplyResources(this.btn_settings, "btn_settings");
+            this.btn_settings.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_settings.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_settings.Name = "btn_settings";
-            this.btn_settings.Size = new System.Drawing.Size(75, 23);
-            this.btn_settings.TabIndex = 13;
-            this.btn_settings.Text = "Settings";
-            this.btn_settings.UseVisualStyleBackColor = true;
+            this.btn_settings.UseVisualStyleBackColor = false;
             this.btn_settings.Click += new System.EventHandler(this.btn_settings_Click);
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 344);
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.Controls.Add(this.btn_settings);
             this.Controls.Add(this.btn_logout);
             this.Controls.Add(this.lbl_stock_management);
@@ -198,7 +161,8 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Main";
-            this.Text = "Dashboard";
+            this.TransparencyKey = System.Drawing.SystemColors.WindowFrame;
+            this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
--- SchoolLibraryStockManagement/Main.cs
+++ SchoolLibraryStockManagement/Main.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace SchoolLibraryStockManagement
 {
     public partial class Main : Form
     {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+   (
+       int nLeftRect,     // x-coordinate of upper-left corner
+       int nTopRect,      // y-coordinate of upper-left corner
+       int nRightRect,    // x-coordinate of lower-right corner
+       int nBottomRect,   // y-coordinate of lower-right corner
+       int nWidthEllipse, // height of ellipse
+       int nHeightEllipse // width of ellipse
+   );
+
+
         public Main()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
         }
 
         private void btn_logout_Click(object sender, EventArgs e)
@@ -57,6 +74,11 @@ namespace SchoolLibraryStockManagement
         private void btn_settings_Click(object sender, EventArgs e)
         {
             new Settings().ShowDialog();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
