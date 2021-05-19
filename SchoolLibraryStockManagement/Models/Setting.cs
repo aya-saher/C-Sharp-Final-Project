79840c2aaba4ccdf5b4d97236fb9887ba705dd51
khawla <khawla.alhodiry@gmail.com>
2021-05-18 23:05:02 -0700

    settings

---
+++ SchoolLibraryStockManagement/Models/Setting.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLibraryStockManagement.Models
+{
+    class Setting
+    {
+        public string all()
+        {
+            return "SELECT * FROM users ";
+        }
+        public string authenticate(string username, string password)
+        {
+            string query = "SELECT* FROM users WHERE username LIKE '%" + username + "%' AND password LIKE '%" + password + "%' ";
+
+            return query;
+
+        }
+        public string updateAll(string oldusername, string username, string name, string password)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("username = '{0}',name = '{1}',password = '{2}' ", username, name, password);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_nameandusername(string oldusername, string username, string name)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("username = '{0}',name = '{1}' ", username, name);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_NameAndPassword(string oldusername, string name, string password)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("name = '{0}',password = '{1}' ", name, password);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_UsernameAndPassword(string oldusername, string username, string password)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("username = '{0}',password = '{1}' ", username, password);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_Name(string oldusername, string name)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("name = '{0}' ", name);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_Username(string oldusername, string username)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("username = '{0}' ", username);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+        public string update_Password(string oldusername, string password)
+        {
+            string query = "UPDATE users SET ";
+            query += String.Format("password = '{0}' ", password);
+            query += String.Format(" WHERE username ='{0}' ", oldusername);
+            return query;
+        }
+    }
+}
+
--- SchoolLibraryStockManagement/Settings.Designer.cs
+++ SchoolLibraryStockManagement/Settings.Designer.cs
@@ -28,55 +28,268 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_password_update = new System.Windows.Forms.Button();
-            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
             this.lbl_password = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox_name = new System.Windows.Forms.CheckBox();
+            this.txtbox_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_repeatname = new System.Windows.Forms.TextBox();
+            this.checkBox_username = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbox_username = new System.Windows.Forms.TextBox();
+            this.txtbox_repeatusername = new System.Windows.Forms.TextBox();
+            this.checkBox_password = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtbox_newpass = new System.Windows.Forms.TextBox();
+            this.txtbox_repeatpass = new System.Windows.Forms.TextBox();
+            this.btn_password_update = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_password_update
+            // txt_username
             // 
-            this.btn_password_update.Location = new System.Drawing.Point(63, 119);
-            this.btn_password_update.Name = "btn_password_update";
-            this.btn_password_update.Size = new System.Drawing.Size(90, 23);
-            this.btn_password_update.TabIndex = 8;
-            this.btn_password_update.Text = "Update";
-            this.btn_password_update.UseVisualStyleBackColor = true;
-            // 
-            // txt_password
-            // 
-            this.txt_password.Location = new System.Drawing.Point(12, 81);
-            this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(141, 20);
-            this.txt_password.TabIndex = 7;
+            this.txt_username.Location = new System.Drawing.Point(140, 57);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(187, 22);
+            this.txt_username.TabIndex = 7;
             // 
             // lbl_password
             // 
             this.lbl_password.AutoSize = true;
-            this.lbl_password.Location = new System.Drawing.Point(9, 65);
+            this.lbl_password.Location = new System.Drawing.Point(35, 114);
+            this.lbl_password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(53, 13);
+            this.lbl_password.Size = new System.Drawing.Size(69, 17);
             this.lbl_password.TabIndex = 6;
             this.lbl_password.Text = "Password";
             // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(140, 109);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(187, 22);
+            this.txt_password.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Username";
+            // 
+            // checkBox_name
+            // 
+            this.checkBox_name.AutoSize = true;
+            this.checkBox_name.Location = new System.Drawing.Point(48, 167);
+            this.checkBox_name.Name = "checkBox_name";
+            this.checkBox_name.Size = new System.Drawing.Size(257, 21);
+            this.checkBox_name.TabIndex = 11;
+            this.checkBox_name.Text = "DO you want to change your name?";
+            this.checkBox_name.UseVisualStyleBackColor = true;
+            // 
+            // txtbox_name
+            // 
+            this.txtbox_name.Location = new System.Drawing.Point(160, 211);
+            this.txtbox_name.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_name.Name = "txtbox_name";
+            this.txtbox_name.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_name.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 216);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "New Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 254);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Repeat Name";
+            // 
+            // txtbox_repeatname
+            // 
+            this.txtbox_repeatname.Location = new System.Drawing.Point(160, 254);
+            this.txtbox_repeatname.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_repeatname.Name = "txtbox_repeatname";
+            this.txtbox_repeatname.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_repeatname.TabIndex = 15;
+            // 
+            // checkBox_username
+            // 
+            this.checkBox_username.AutoSize = true;
+            this.checkBox_username.Location = new System.Drawing.Point(42, 292);
+            this.checkBox_username.Name = "checkBox_username";
+            this.checkBox_username.Size = new System.Drawing.Size(285, 21);
+            this.checkBox_username.TabIndex = 16;
+            this.checkBox_username.Text = "DO you want to change your username?";
+            this.checkBox_username.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 332);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "New Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 361);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Repeat Username";
+            // 
+            // txtbox_username
+            // 
+            this.txtbox_username.Location = new System.Drawing.Point(170, 327);
+            this.txtbox_username.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_username.Name = "txtbox_username";
+            this.txtbox_username.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_username.TabIndex = 19;
+            // 
+            // txtbox_repeatusername
+            // 
+            this.txtbox_repeatusername.Location = new System.Drawing.Point(170, 358);
+            this.txtbox_repeatusername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_repeatusername.Name = "txtbox_repeatusername";
+            this.txtbox_repeatusername.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_repeatusername.TabIndex = 20;
+            // 
+            // checkBox_password
+            // 
+            this.checkBox_password.AutoSize = true;
+            this.checkBox_password.Location = new System.Drawing.Point(38, 407);
+            this.checkBox_password.Name = "checkBox_password";
+            this.checkBox_password.Size = new System.Drawing.Size(282, 21);
+            this.checkBox_password.TabIndex = 21;
+            this.checkBox_password.Text = "DO you want to change your password?\r\n";
+            this.checkBox_password.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 448);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "New password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 480);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 17);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Repeat password";
+            // 
+            // txtbox_newpass
+            // 
+            this.txtbox_newpass.Location = new System.Drawing.Point(170, 443);
+            this.txtbox_newpass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_newpass.Name = "txtbox_newpass";
+            this.txtbox_newpass.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_newpass.TabIndex = 24;
+            // 
+            // txtbox_repeatpass
+            // 
+            this.txtbox_repeatpass.Location = new System.Drawing.Point(170, 477);
+            this.txtbox_repeatpass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtbox_repeatpass.Name = "txtbox_repeatpass";
+            this.txtbox_repeatpass.Size = new System.Drawing.Size(187, 22);
+            this.txtbox_repeatpass.TabIndex = 25;
+            // 
+            // btn_password_update
+            // 
+            this.btn_password_update.Location = new System.Drawing.Point(125, 535);
+            this.btn_password_update.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_password_update.Name = "btn_password_update";
+            this.btn_password_update.Size = new System.Drawing.Size(171, 28);
+            this.btn_password_update.TabIndex = 8;
+            this.btn_password_update.Text = "Update";
+            this.btn_password_update.UseVisualStyleBackColor = true;
+            this.btn_password_update.Click += new System.EventHandler(this.btn_password_update_Click);
+            // 
             // Settings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(198, 261);
-            this.Controls.Add(this.btn_password_update);
+            this.ClientSize = new System.Drawing.Size(415, 576);
+            this.Controls.Add(this.txtbox_repeatpass);
+            this.Controls.Add(this.txtbox_newpass);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox_password);
+            this.Controls.Add(this.txtbox_repeatusername);
+            this.Controls.Add(this.txtbox_username);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox_username);
+            this.Controls.Add(this.txtbox_repeatname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbox_name);
+            this.Controls.Add(this.checkBox_name);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.btn_password_update);
+            this.Controls.Add(this.txt_username);
             this.Controls.Add(this.lbl_password);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Settings";
             this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_password_update;
-        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_username;
         private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox_name;
+        private System.Windows.Forms.TextBox txtbox_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_repeatname;
+        private System.Windows.Forms.CheckBox checkBox_username;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbox_username;
+        private System.Windows.Forms.TextBox txtbox_repeatusername;
+        private System.Windows.Forms.CheckBox checkBox_password;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtbox_newpass;
+        private System.Windows.Forms.TextBox txtbox_repeatpass;
+        private System.Windows.Forms.Button btn_password_update;
     }
 }
